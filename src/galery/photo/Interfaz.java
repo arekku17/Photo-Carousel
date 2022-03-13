e08713491be48008d042cbb09659cbf3672b46ea
Alejandro <jose_alex170702@hotmail.com>
2022-03-13 14:19:53 -0600

    V1

--- src/galery/photo/Interfaz.java
+++ src/galery/photo/Interfaz.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package galery.photo;
 
 import java.io.File;
@@ -12,10 +7,6 @@ import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
-/**
- *
- * @author theto
- */
 public class Interfaz extends javax.swing.JFrame {
 
     Galeria controlGaleria;
